7b5940757c2590606fa8dfb7f6cf5f794e5374f2
Alok <app220@nist.edu>
2022-08-01 16:47:52 +0530

    FromAlok

--- controllers/book.js
+++ controllers/book.js
@@ -11,6 +11,10 @@ exports.getAllBook= async (req, res) => {
         console.log(error);
     }
 }
+exports.createBook=(req,res,next)=>{
+  
+
+}
 
 // const orderid = (req,res)=>{
 //     res.send("Getting id");
@@ -21,14 +25,14 @@ exports.getAllBook= async (req, res) => {
 //};
 
 
-exports.createBook =async (req,res)=>{
+/*exports.createBook =async (req,res)=>{
     // const {customerId} = req.query;
     // const body = req.body;
     const bookPost=await BookService.createBook();
     console.log(bookPost);
     res.send(bookPost);
     // return BookService.createBook();
-}
+}*/
 exports.deleteBook=async(req,res)=>{
     const id=req.params.id;
     console.log(id);
--- routes/book.js
+++ routes/book.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 const bookcontroller = require('../controllers/book');
 
-//router.get('/',controller.getStudents);
+router.get('/',bookcontroller.getAllBook);
 
 // router.get('/view',controller.orderid);
+
 router.post('/create',bookcontroller.createBook);
 router.delete('/delete/:id',bookcontroller.deleteBook);
 router.put('/edit',bookcontroller.editBook);
